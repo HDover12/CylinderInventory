d27c59669621d8a024e3ede5ad3d7a00a651448f
Hannah <hmguinn@gmail.com>
2022-10-16 17:36:32 -0400

    Added tooltip and description column

---
+++ BookApp/Migrations/20221016205341_AddDesciptionColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookApp.Migrations
+{
+    public partial class AddDesciptionColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "CylinderInventory",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "CylinderInventory");
+        }
+    }
+}
